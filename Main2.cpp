0ae7731627f4b271e56dde502a8971ed575e8801
hari196 <hharitha239@gmail.com>
2024-11-12 15:15:32 +0530

    Update Main2.cpp

--- Main2.cpp
+++ Main2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello,my World!" << std::endl;
     std::cout << "Hello123" << std::endl;
     std::cout << "Hii, Byee" << std::endl;
     return 0;
