0516464f8b5bc89424b00129ba1778cacda073a6
hari196 <hharitha239@gmail.com>
2024-11-12 15:15:10 +0530

    Add files via upload

---
+++ Main2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello123" << std::endl;
+    std::cout << "Hii, Byee" << std::endl;
+    return 0;
+}
